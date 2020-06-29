759f9863476b3fa4fef939a72c9a968a7b83b946
hiromi-mitsuoka <hiro.19981217@ezweb.ne.jp>
2020-06-30 03:13:23 +0900

    first commit

--- sketch.js
+++ sketch.js
@@ -1,85 +1,75 @@
-class Point3D {
-  constructor(pos, vel, size) {
-    this._pos = pos;
-    this._vel = vel;
+class PointSphere {
+  constructor(size = 1.0, num = 100, radius = 100) {
     this._size = size;
+    this._num = num;
+    this._radius = radius;
+    this._pos = [this._num];
+    this._rNoise = [this._num];
+    this.setPos();
   }
-
+  setPos() {
+    for (let i = 0; i < this._num; i++) {
+      this._ang = degrees(random(360));
+      this._posZ = random(-1, 1);
+      this._posX = sqrt(1 - this._posZ * this._posZ) * cos(this._ang);
+      this._posY = sqrt(1 - this._posZ * this._posZ) * sin(this._ang);
+      this._pos[i] = createVector(this._posX, this._posY, this._posZ);
+      this._pos[i].mult(this._radius);
+    }
+  }
   move() {
-    if (this._pos.x < -boxSize / 2 + this._size / 2 || this._pos.x > boxSize / 2 - this._size / 2) {
-      this._vel.x *= -1;
+    for (let i = 0; i < this._num; i++) {
+      this._rNoise[i] = createVector(random(-0.5, 0.5), random(-0.5, 0.5), random(-0.5, 0.5));
+      this._pos[i].add(this._rNoise[i]);
     }
-    if (this._pos.y < -boxSize / 2 + this._size / 2 || this._pos.y > boxSize / 2 - this._size / 2) {
-      this._vel.y *= -1;
-    }
-    if (this._pos.z < -boxSize / 2 + this._size / 2 || this._pos.z > boxSize / 2 - this._size / 2) {
-      this._vel.z *= -1;
-    }
-
-    this._pos.add(this._vel);
   }
-
-  changeColor() { }
-
-  update() {
-    this.move();
-  }
-
   draw() {
-    stroke(255);
-    strokeWeight(this._size);
-    translate(0, 0, 0);
-    point(this._pos.x, this._pos.y, this._pos.z);
-  }
-
-  get pos() {
-    return this._pos;
-  }
-
-  set size(value) {
-    this._size = value;
+    for (let i = 0; i < this._num; i++) {
+      push();
+      translate(this._pos[i]);
+      sphere(this._size);
+      pop();
+      // strokeWeight(this._size);
+      // point(this._pos[i]);
+    }
   }
 }
 
 
-// Points3D
-let points3D = [];
-const point3DNum = 100;
-let pointScale;
-let velScale = 3.0;
-// box
-let boxSize = 200;
+let PointsSphere;
 
 function setup() {
+  frameRate(30);
   createCanvas(500, 500, WEBGL);
-  perspective(PI / 3.0, width / height, 0.1, 800);
-  // Points3D
-  pointScale = boxSize / 2;
-  for (let i = 0; i < point3DNum; i++) {
-    let pos = createVector(random() * 2.0 - 1.0, random() * 2.0 - 1.0, random() * 2.0 - 1.0); //-1~1
-    pos.mult(pointScale);
-    let vel = createVector(random() * 2.0 - 1.0, random() * 2.0 - 1.0, random() * 2.0 - 1.0); //-1~1
-    vel.mult(velScale);
-    let size = random(20);
-    points3D[i] = new Point3D(pos, vel, size);
-  }
+  // stroke(0);
+  noStroke();
+
+  PointsSphere = new PointSphere(2.0, 500, 50);
 }
 
 function draw() {
-  background(10);
-  // camera
+  background(0);
   orbitControl();
-  directionalLight(200, 200, 200, 0, 0, 400);
+  directionalLight(200, 200, 200, 100, 300, -300, 300);
   ambientLight(200);
-  rotateY(frameCount * 0.005);
+  pointLight(200, 10, 200, 200, 0, 0, 0);
+  perspective(PI / 3.0, width / height, 0.1, 800);
+  camera(300 * cos(frameCount * 0.02), 100 * sin(frameCount * 0.01), 300 * sin(frameCount * 0.02), 0, 0, 0, 0, 1.0, 0);
+  specularMaterial(200, 10, 200, 200);
 
-  // Points3D
-  for (let i = 0; i < point3DNum; i++) {
-    points3D[i].update();
-    points3D[i].draw();
-  }
-  // box
-  fill(255, 10);
-  strokeWeight(1.0);
-  box(boxSize);
+  //PointSphere
+  PointsSphere.move();
+  PointsSphere.draw();
+
+  //box
+  push();
+  translate(0, 0, 0);
+  fill(240, 50);
+  stroke(250);
+  rotateZ(PI / 4);
+  box(150);
+  line(0, 0, 0, 500, 0, 0);
+  line(0, 0, 0, 0, -500, 0);
+  line(0, 0, 0, 0, 0, 500);
+  pop();
 }
